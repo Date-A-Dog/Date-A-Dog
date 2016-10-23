c465df5bfcd60e4a6c20e303c3ac47d5f0691ed8
Amanda <amandaloh16@yahoo.com>
2016-10-23 13:34:18 -0700

    Added the implmentation for the date request class

--- app/src/main/java/dateadog/dateadog/DateRequest.java
+++ app/src/main/java/dateadog/dateadog/DateRequest.java
@@ -1,5 +1,45 @@
 package dateadog.dateadog;
+/**
+ * A DateRequest is a user submission of what time and what dog they want to have a date with
+ * @author Amanda Loh
+ * @version 1.0
+ */
+
+import Date;
 
 public class DateRequest {
+    private int dogID;
+    private Date date;
+    private int requestID;
+    private char status;
+    private String token;
+
+    /**
+     * Constructor
+     * @param dogId identifies the dog
+     * @param date the time fo the date for the dog
+     * @param requestID for retrirving the date request from the database
+     */
+    public DateRequest(int dogId, Date date, int requestID, char status, String token) {
+        this.dogID = dogID;
+        this.date = date;
+        this.requestID = requestID;
+        this.status = status;
+        this.token = token;
+
+    }
+
+    /**
+     * Getters
+     */
+    int getDogID() {
+        return this.dogID;
+    }
+
+    long getEpoch() {
+        return this.epoch;
+    }
+
+
 
 }

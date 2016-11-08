33b40003ba99c64d92e990b9b6eeb088aab88780
laurenwolfe <lulumialu@gmail.com>
2016-11-08 13:13:24 -0800

    Query integration

--- api/controllers/queries.js
+++ api/controllers/queries.js
@@ -19,33 +19,6 @@ var cn = {
 var db = pgp(cn);
 
 // add query functions
-function getNextDogs(req, res, next) {
-  var query = 'SELECT d.dog_id, d.dog_name, d.age, d.sex, d.size, di.image_url, s.shelter_name \
-               FROM Dogs d \
-               JOIN DogImages di ON d.dog_id = di.dog_id \
-               JOIN Shelters s ON d.shelter_id = s.shelter_id \
-               WHERE NOT EXISTS (SELECT vd.dog_id \
-                 FROM ViewedDogs vd \
-                 WHERE vd.user_id = 2 \
-                 AND d.dog_id = vd.dog_id) \
-               AND di.image_id = 1 \
-               AND di.image_size = $1 \
-               ORDER BY d.dog_id ASC \
-               LIMIT 1';
-  db.any(query, ['pn'])
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ALL doggies'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
-
 function getDogSummary(req, res, next) {
     var query = 'SELECT d.dog_id, d.dog_name, d.age, d.sex, d.size, di.image_url, s.shelter_name \
     FROM Dogs d \
@@ -54,8 +27,8 @@ function getDogSummary(req, res, next) {
     WHERE d.dog_id = $1 \
     AND di.image_id = $2 \
     AND di.image_size = $3 \
-    LIMIT 1';
-    db.one(query, [req.body.dog_id, '1', 'pn'])
+    LIMIT $4';
+    db.one(query, [req.body.dog_id, 1, 'pn', 1])
         .then(function (data) {
             res.status(200)
                 .json({
@@ -78,8 +51,8 @@ function getDogDetail(req, res, next) {
                 WHERE d.dog_id = $1 \
                 AND di.image_id = $2 \
                 AND di.image_size = $3 \
-                LIMIT 1';
-    db.one(query, [req.body.dog_id, '1', 'pn'])
+                LIMIT $4';
+    db.one(query, [req.body.dog_id, 1, 'pn', 1])
         .then(function (data) {
             res.status(200)
                 .json({
@@ -104,7 +77,7 @@ function getLikedDogs(req, res, next) {
     AND di.image_size = $4 \
     ORDER BY vd.liked_time DESC';
 
-    db.any(query, [req.body.user_id, TRUE, 1, 'fpm'])
+    db.any(query, [req.user.id, 'TRUE', 1, 'fpm'])
         .then(function (data) {
             res.status(200)
                 .json({
@@ -122,12 +95,11 @@ function makeDateRequest(req, res, next) {
     var query = 'INSERT INTO Request (dog_id, shelter_id, user_id, status, requested_date) \
     VALUES ($1, $2, $3, $4, $5)';
 
-    db.none(query, [req.body.dog_id, req.body.shelter_id, req.body.user_id, 'A', req.body.date])
-        .then(function (data) {
+    db.none(query, [req.body.dog_id, req.body.shelter_id, req.user.id, 'A', req.body.date])
+        .then(function () {
             res.status(200)
                 .json({
                     status: 'success',
-                    data: data,
                     message: 'Added Date Request'
                 });
         })
@@ -147,12 +119,11 @@ function updateUserApplication(req, res, next) {
     WHERE user_id = $7';
 
     db.none(query, [req.body.full_name, req.body.street, req.body.city, req.body.state,
-        req.body.zipcode, req.body.phone, req.body.user_id])
-        .then(function (data) {
+        req.body.zipcode, req.body.phone, req.user.id])
+        .then(function () {
             res.status(200)
                 .json({
                     status: 'success',
-                    data: data,
                     message: 'Added Date Request'
                 });
         })
@@ -216,11 +187,10 @@ function updateDateRequestStatus(req, res, next) {
     WHERE req_id = $2';
 
     db.none(query, [req.body.status, req.body.req_id])
-        .then(function (data) {
+        .then(function () {
             res.status(200)
                 .json({
                     status: 'success',
-                    data: data,
                     message: 'Updated Date Request Status'
                 });
         })
@@ -231,7 +201,7 @@ function updateDateRequestStatus(req, res, next) {
 
 module.exports = {
   // Rest API specific queries
-  getNextDogs: getNextDogs,
+  //getNextDogs: getNextDogs,
   // judgeDog: judgeDog,
     getDogSummary: getDogSummary,
     getDogDetail: getDogDetail,

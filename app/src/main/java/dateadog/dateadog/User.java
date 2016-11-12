b69506186c8ffdeee8fab5dfe476c332f0b28d08
Hassan Abdi <habdi@cs.washington.edu>
2016-11-11 16:18:16 -0800

    Implemented Unit test for User, DateRequest

--- app/src/main/java/dateadog/dateadog/User.java
+++ app/src/main/java/dateadog/dateadog/User.java
@@ -1,4 +1,44 @@
 package dateadog.dateadog;
 
+/**
+ * A {@code User} is a profile of a user.
+ */
 public class User {
+
+    // Facebook authentication
+    private String authToken;
+
+    // User's name
+    private String name;
+
+
+
+    /**
+     * Constructs and initializes a new {@code User} with the given data.
+     *
+     * @param authToken the user's facebook authentication
+     * @param name the user's name
+     */
+    public User(String authToken, String name)  {
+        this.authToken = authToken;
+        this.name = name;
+    }
+
+    /**
+     * Returns a String that uniquely identifies this {@code User}
+     *
+     * @return a String that uniquely identifies this {@code User}
+     */
+    public String getUserToken() {
+        return this.authToken;
+    }
+
+    /**
+     * Returns a string that represents the user's name
+     *
+     * @return a String that represents the user's name
+     */
+    public String getUserName() {
+        return this.name;
+    }
 }

b8f3c93cbe5c0c48c6a6d018cf8acf1b63991b6d
Hassan Abdi <habdi@cs.washington.edu>
2016-11-15 20:54:23 -0800

    Implemented ConfirmRequestActivity

---
+++ app/src/main/java/dateadog/dateadog/DatePickerFragment.java
@@ -0,0 +1,56 @@
+package dateadog.dateadog;
+
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.DatePicker;
+
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DatePickerFragment} factory method to
+ * create an instance of this fragment.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+
+    private DatePicker datePicker;
+
+    public interface DateDialogListener {
+        void onFinishDialog(Date date);
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.fragment_date_picker2,null);
+        datePicker = (DatePicker) v.findViewById(R.id.dialog_date_date_picker);
+        return new android.support.v7.app.AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle("Select Date")
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                int year = datePicker.getYear();
+                                int mon = datePicker.getMonth();
+                                int day = datePicker.getDayOfMonth();
+                                Date date = new GregorianCalendar(year,mon,day).getTime();
+                                DateDialogListener activity = (DateDialogListener) getActivity();
+                                activity.onFinishDialog(date);
+                                dismiss();
+                            }
+                        })
+                .create();
+    }
+}

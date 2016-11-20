ccbad0b3289df05e732994020e4eb750b2080873
Ubuntu <ubuntu@ip-10-0-0-161.us-west-2.compute.internal>
2016-11-20 03:05:52 +0000

    zipcode function

--- api/controllers/queries.js
+++ api/controllers/queries.js
@@ -158,6 +158,23 @@ function getShelterRequestsDemo(req, res, next) {
     });
 }
 
+function getZipcodes(zipcode, distance) {
+  var PythonShell = require('python-shell');
+
+  var options = {
+    mode: 'text',
+    pythonPath: '/usr/bin/python',
+    scriptPath: '../zipcodes/zipcode_tools',
+    args: [zipcode, distance]
+  };
+
+  PythonShell.run('main.py', options, function(err, res){
+    if(err) throw err;
+    return res;
+  });
+}
+
+
 module.exports = {
   // Rest API specific queries
   login: login,

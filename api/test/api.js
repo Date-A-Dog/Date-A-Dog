541c761e6c1a50e4be4db584e29add814ca18429
Amarpal Singh <amarpal@cs.washington.edu>
2016-11-21 02:00:34 -0800

    Attempt at verbose implementation of login test

--- api/test/api.js
+++ api/test/api.js
@@ -7,59 +7,83 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe("Date-a-Dog Server", function() {
-  describe("Rest API tests", function() {
-    it('Should return user profile for test user on /api/loginTest', function(done) {
-      chai.request(server)
-      .post('/api/loginTest')
-      .end(function(err, res){
+describe("Date-a-Dog Server Rest API Tests", function() {
+  describe("Should return user profile for test user on /api/loginTest", function() {
+    chai.request(server)
+    .post('/api/loginTest')
+    .end(function(err, res){
+      it('Response is OK', function(done) {
         res.should.have.status(200);
+      });
+
+      it('Response is a valid JSON', function(done) {
         res.should.be.json;
         res.body.should.be.a('object');
+      });
 
-        // Check id
+      // Check id
+      it('id is correct', function(done) {
         res.body.should.have.property('id');
         res.body.id.should.equal(119889308491710);
+      });
 
-        // Check email
+      // Check email
+      it('email is correct', function(done) {
         res.body.should.have.property('email');
         res.body.email.should.equal('dateadog@gmail.com');
+      });
 
-        // Check fname
+      // Check fname
+      it('fname is correct', function(done) {
         res.body.should.have.property('fname');
         res.body.fname.should.equal('Sally');
+      });
 
-        // Check lname
+      // Check lname
+      it('lname is correct', function(done) {
         res.body.should.have.property('lname');
         res.body.lname.should.equal('Smith');
+      });
 
-        // Check street
+
+      // Check street
+      it('street is correct', function(done) {
         res.body.should.have.property('street');
         res.body.street.should.equal('Paul G. Allen Center for Computer Science & Engineering (Cse)');
+      });
 
-        // Check city
+      // Check city
+      it('city is correct', function(done) {
         res.body.should.have.property('city');
         res.body.city.should.equal('Seattle');
+      });
 
-        // Check state
+
+      // Check state
+      it('state is correct', function(done) {
         res.body.should.have.property('state');
         res.body.state.should.equal('WA');
+      });
 
-        // Check zip
+      // Check zip
+      it('zip is correct', function(done) {
         res.body.should.have.property('zip');
         res.body.zip.should.equal('98195');
+      });
 
-        // Check phone
+      // Check phone
+      it('phone is correct', function(done) {
         res.body.should.have.property('phone');
         res.body.phone.should.equal('(206) 543-1695');
+      });
 
-        // Check shelterid
+      // Check shelterid
+      it('fname is correct', function(done) {
         res.body.should.have.property('shelterid');
         res.body.shelterid.should.equal('WA214');
-        done();
       });
+
+      done();
     });
-
-
   });
 });

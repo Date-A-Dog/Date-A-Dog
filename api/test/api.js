06444d4368da5b7f386db10b281391a580c6cb1f
Amarpal Singh <amarpal@cs.washington.edu>
2016-11-21 01:46:14 -0800

    Update to login test

--- api/test/api.js
+++ api/test/api.js
@@ -16,10 +16,50 @@ describe("Date-a-Dog Server", function() {
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.a('object');
+
+        // Check id
         res.body.should.have.property('id');
         res.body.id.should.equal(119889308491710);
+
+        // Check email
+        res.body.should.have.property('email');
+        res.body.id.should.equal('dateadog@gmail.com');
+
+        // Check fname
+        res.body.should.have.property('fname');
+        res.body.id.should.equal('Sally');
+
+        // Check lname
+        res.body.should.have.property('lname');
+        res.body.id.should.equal('Smith');
+
+        // Check street
+        res.body.should.have.property('street');
+        res.body.id.should.equal('Paul G. Allen Center for Computer Science & Engineering (Cse)');
+
+        // Check city
+        res.body.should.have.property('city');
+        res.body.id.should.equal('Seattle');
+
+        // Check state
+        res.body.should.have.property('state');
+        res.body.id.should.equal('WA');
+
+        // Check zip
+        res.body.should.have.property('zip');
+        res.body.id.should.equal('98195');
+
+        // Check phone
+        res.body.should.have.property('phone');
+        res.body.id.should.equal('(206) 543-1695');
+
+        // Check shelterid
+        res.body.should.have.property('shelterid');
+        res.body.id.should.equal('WA214');
         done();
       });
     });
+
+    
   });
 });

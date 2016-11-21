5ff7322dfacde77736729f263945a47c5029ab9c
Amarpal Singh <amarpal@cs.washington.edu>
2016-11-21 06:04:48 -0800

    Fixing test for /api/judgeDog

--- api/controllers/queries.js
+++ api/controllers/queries.js
@@ -160,6 +160,7 @@ function getShelter(req, res, next) {
 }
 
 function judgeDog(req, res, next) {
+  console.log(req.body);
   var query = 'INSERT INTO judged (userId, dogId, epoch, liked) \
                VALUES ($1, $2, $3, $4) \
                ON CONFLICT (userId, dogId) \

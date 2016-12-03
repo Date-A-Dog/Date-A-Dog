3882f91c836b6019f976c36e95942f6eaf1bd4d3
Anmol Jammu <jammua@cs.washington.edu>
2016-12-02 22:41:23 -0800

    Fixed back button issue

--- app/src/main/java/dateadog/dateadog/MainActivity.java
+++ app/src/main/java/dateadog/dateadog/MainActivity.java
@@ -133,6 +133,14 @@ public class MainActivity extends AppCompatActivity implements LikedDogsFragment
     }
 
     @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
     public void onFragmentInteraction(Uri uri) {
         Log.i(TAG, uri.toString());
     }

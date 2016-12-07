0cba61b34affba0d17af2b85ac9cb52ea1490098
Anmol Jammu <jammua@cs.washington.edu>
2016-12-06 20:05:04 -0800

    Added loading spinner

--- app/src/main/java/dateadog/dateadog/LikedDogsRecyclerViewAdapter.java
+++ app/src/main/java/dateadog/dateadog/LikedDogsRecyclerViewAdapter.java
@@ -2,6 +2,9 @@ package dateadog.dateadog;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +13,14 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.DrawableRequestBuilder;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.FutureTarget;
 
+import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class LikedDogsRecyclerViewAdapter extends RecyclerView.Adapter {
 
@@ -59,10 +67,37 @@ public class LikedDogsRecyclerViewAdapter extends RecyclerView.Adapter {
         if (position >= likedDogs.size()) {
             return;
         }
-        ViewHolder viewHolder = (ViewHolder) holder;
+        final ViewHolder viewHolder = (ViewHolder) holder;
         viewHolder.dog = likedDogs.get(position);
         viewHolder.name.setText(likedDogs.get(position).getName());
-        Glide.with(activity).load(likedDogs.get(position).getImage()).into(viewHolder.cardImage);
+
+        final FutureTarget<GlideDrawable> request = Glide.with(activity)
+                .load(likedDogs.get(position).getImageURL()).centerCrop()
+                .into(viewHolder.cardImage.getWidth(), viewHolder.cardImage.getHeight());
+        new AsyncTask<Void, Void, GlideDrawable>() {
+            @Override
+            protected void onPreExecute() {
+                viewHolder.cardImage.setImageResource(R.drawable.loading_spinner);
+            }
+
+            @Override
+            protected GlideDrawable doInBackground(Void... voids) {
+                try {
+                    return request.get();
+                } catch (InterruptedException e) {
+
+                } catch (ExecutionException e) {
+
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(GlideDrawable glideDrawable) {
+                viewHolder.cardImage.setImageDrawable(glideDrawable);
+            }
+        }.execute();
+
         viewHolder.layout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
